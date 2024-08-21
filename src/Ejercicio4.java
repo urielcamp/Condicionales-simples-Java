2852af5f5f58ca5513fd5ed25f37be59568c0dd6
Uriel Campaña <urielcamp@gmail.com>
2024-08-21 14:42:49 -0300

    ejercicio 4

---
+++ src/Ejercicio4.java
@@ -0,0 +1,18 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+
+        int num = 2;
+        int otroNum = 4;
+
+        if (otroNum%num == 0){
+            System.out.println("es multiplo");
+        }else {
+            System.out.println("no es multiplo");
+        }
+
+
+
+    }
+
+
+}
