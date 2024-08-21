2458f28046460a31a77effe39efd8752b98de6bb
Uriel Campaña <urielcamp@gmail.com>
2024-08-21 14:18:45 -0300

    ejercicio 2

---
+++ src/Ejercicio2.java
@@ -0,0 +1,21 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        int numA = 4;
+        int numB = 2;
+
+        if (numA > numB){
+
+            System.out.println(Math.pow(numA, numB));
+        }else{
+            System.out.println(Math.pow(numB, numA));
+        }
+
+
+
+
+    }
+
+
+
+}
