36790f3cce4ba0a307b018f34d2f3c1d215e00e3
Uriel Campaña <urielcamp@gmail.com>
2024-08-21 14:32:29 -0300

    ejercicio 3

---
+++ src/Ejercicio3.java
@@ -0,0 +1,12 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        int num = -9;
+
+        if (num < 0) {
+            System.out.println(num + " es negativo");
+        }
+
+
+    }
+}
