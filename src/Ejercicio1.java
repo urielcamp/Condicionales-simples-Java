5e9c61d2a76272ccbc02fe0539ba0e8be8bc813e
Uriel Campaña <urielcamp@gmail.com>
2024-08-21 12:02:42 -0300

    primer ejercicio

---
+++ src/Ejercicio1.java
@@ -0,0 +1,18 @@
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+
+        double num = 1 + (100 - 1) * Math.random();
+
+        if (num >= 50){
+            System.out.println(num + " es mayor a 50");
+        }else{
+            System.out.println(num + " es menor a 50");
+        }
+
+    }
+
+
+
+}
